2524be9d8bbbc94d20471a0564071abb08d36f92
NX0302 <91772790+NX0302@users.noreply.github.com>
2023-02-17 10:27:32 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "This is PES2UG20CS223\n";
+  cout << "Execution of task5\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
